e33f1dd19354048f41cf9ab91fb2e6876436edab
Basel-Abd-Alsous <basel.alsous2000@gmail.com>
2025-11-18 00:25:09 +0300

    Completed Notification And Otp

--- lib/presentation/auth/domain/model/param/otp_param.dart
+++ lib/presentation/auth/domain/model/param/otp_param.dart
@@ -1,7 +1,7 @@
 class OTPParam {
   final String? mobileNumber;
   final int? otp;
-
+  //ss
   OTPParam({this.mobileNumber, this.otp});
   OTPParam.fromJson(Map<String, dynamic> json) : mobileNumber = json['phone'] as String?, otp = json['otp'] as int?;
   Map<String, dynamic> toJson() => {'phone': mobileNumber, 'otp': otp};
